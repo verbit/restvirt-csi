35885be7450012cdd7ae8713715126b732dec18e
Ilya Verbitskiy <ilya@verbit.io>
2021-03-07 22:06:51 +0100

    Fix CLI parsing

--- main.go
+++ main.go
@@ -3,27 +3,36 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 )
 
 func main() {
-	var address string
-	flag.StringVar(&address, "csi-address", "", "test me bro")
-	var apiEndpoint string
-	flag.StringVar(&apiEndpoint, "api-endpoint", "http://localhost:8090", "test me bro")
-
 	flag.Set("logtostderr", "true")
 
-	flag.Parse()
+	if len(os.Args) < 2 {
+		log.Fatalln(`you must specify "node" or "controller"`)
+	}
+
+	println(os.Args[1])
+
+	var mode string
+	switch os.Args[1] {
+	case "node", "controller":
+		mode = os.Args[1]
+	default:
+		log.Fatalln(`you must specify "node" or "controller"`)
+	}
+
+	fs := flag.NewFlagSet("restvirt-csi", flag.ExitOnError)
+	var address string
+	fs.StringVar(&address, "csi-address", "", "test me bro")
+	var apiEndpoint string
+	fs.StringVar(&apiEndpoint, "api-endpoint", "http://localhost:8090", "test me bro")
+	fs.Parse(os.Args[2:])
 
 	if address == "" {
 		log.Fatalln("csi-address must be set")
 	}
-
-	if flag.NArg() < 1 {
-		log.Fatalln(`you must specify "node" or "controller"`)
-	}
-
-	mode := flag.Arg(0)
 
 	server := NewGRPCServer()
 	server.Run(mode, "unix", address)
